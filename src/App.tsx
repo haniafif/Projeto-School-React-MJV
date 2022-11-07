eb111c83b30af65727824ea846be9c915e42f573
flyonmarketing <haniaboultaif@gmail.com>
2022-11-06 21:14:03 -0300

    First Commit, Basic implement + Theme Toggler Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import useLocalStorage from 'use-local-storage'
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+  
+  const switchTheme = ()=>{
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    console.log(newTheme)
+    setTheme(newTheme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main data-theme = {theme}>
+      <h1>I am a string</h1>
+      <button id='themeToggler' onClick={switchTheme}> Switch Theme </button>
+   </main>
   );
 }
 
