9ff2585d8d03a14c8e1edf2b32949e18cdab2fcb
flyonmarketing <haniaboultaif@gmail.com>
2022-11-06 23:37:59 -0300

    Feature Light/Dark Bulb

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css';
 import useLocalStorage from 'use-local-storage'
+import { useEffect } from 'react';
 
 function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -8,15 +9,37 @@ function App() {
   
   const switchTheme = ()=>{
     const newTheme = theme === 'light' ? 'dark' : 'light'
-    console.log(newTheme)
     setTheme(newTheme)
+
+    if (theme === 'light'){
+      document.getElementById("darkbulb")?.classList.remove("sumir")
+      document.getElementById("darkglow")?.classList.remove("sumir")
+    } else{
+      document.getElementById("lightbulb")?.classList.remove("sumir")
+      document.getElementById("lightglow")?.classList.remove("sumir")
+    }
   }
 
+  useEffect(()=>{
+    if (theme === 'light'){
+      document.getElementById("darkbulb")?.classList.add("sumir")
+      document.getElementById("darkglow")?.classList.add("sumir")
+    } else{
+      document.getElementById("lightbulb")?.classList.add("sumir")
+      document.getElementById("lightglow")?.classList.add("sumir")
+    }
+  },[theme])
+
   return (
-   <main data-theme = {theme}>
+   <body data-theme = {theme}>
       <h1>I am a string</h1>
-      <button id='themeToggler' onClick={switchTheme}> Switch Theme </button>
-   </main>
+
+          <div id='lightglow'></div>
+          <div id='darkglow'></div>
+          <img id='lightbulb' onClick={switchTheme} src="/images/light-bulb.png" alt="" />
+          <img id='darkbulb' onClick={switchTheme} src="/images/dark-bulb.png" alt="" />
+      
+   </body>
   );
 }
 
